a7c33f7a3102d298f1768f722b24f8f133d3aa90
Nursultan <nursultan.abdyrazakov1@gmail.com>
2021-11-26 16:38:21 +0600

    added some animation

---
+++ app.js
@@ -0,0 +1,12 @@
+VANTA.BIRDS({
+    el: "#Home",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color1: 0x75430b,
+    color2: 0xc0640f
+    })
