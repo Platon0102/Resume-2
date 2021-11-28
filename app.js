daca6c9859118231538cebee5214f524454a73a7
Nursultan <nursultan.abdyrazakov1@gmail.com>
2021-11-28 20:56:37 +0600

    Mergeing

---
+++ app.js
@@ -0,0 +1,19 @@
+VANTA.BIRDS({
+    el: "#Home",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0xffffff,
+    color2: 0x2dd9ff,
+    colorMode: "lerp",
+    birdSize: 1,
+    wingSpan: 17.00,
+    speedLimit: 7.00,
+    separation: 57.00,
+    cohesion: 26.00,
+    quantity: 3.00
+  })
